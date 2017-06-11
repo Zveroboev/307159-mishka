063df03bade367063816e1c0242656e48a8749c9
Sergey Zveroboev <szveroboev@gmail.com>
2017-06-11 04:14:08 +0400

    Добавил функционал открытия и закрытия меню через js

---
+++ js/main.js
@@ -0,0 +1,20 @@
+var navToggler = document.querySelector(".header-top__toggler-img");
+
+navToggler.addEventListener('click', openOrCloseMenu );
+
+function openOrCloseMenu() {
+  var element = document.getElementById("main-nav");
+  var typeMenu = element.classList.contains("main-nav--closed");
+
+  if (typeMenu) {
+    element.classList.remove('main-nav--closed');
+    navToggler.setAttribute('src', '/img/icon-menu-close.svg');
+    navToggler.setAttribute('alt', 'Закрыть меню');
+  } else {
+    element.classList.add('main-nav--closed');
+    navToggler.setAttribute('src', '/img/icon-menu-open.svg');
+    navToggler.setAttribute('alt', 'Открыть меню');
+  }
+}
+
+
