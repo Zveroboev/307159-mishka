e9ce0e9b61f4ebcc8d04591bc4f0715b8f5dac07
Sergey Zveroboev <szveroboev@gmail.com>
2017-06-13 13:11:40 +0300

    Добавил функционал открытия/закрытия модального окна

--- js/main.js
+++ js/main.js
@@ -1,10 +1,21 @@
-var navToggler = document.querySelector(".header-top__toggler-img");
+var navToggler = document.querySelector('.header-top__toggler-img');
+var promoButton = document.querySelector('.week-item__btn');
+var modalButton = document.querySelector('.modal__form-send');
+var modalWrapper = document.querySelector('.modal');
 
-navToggler.addEventListener('click', openOrCloseMenu );
+//Добавить в функцию при загрузке страницы
+promoButton.setAttribute('href', '#');
+promoButton.removeAttribute('target');
+//
+
+navToggler.addEventListener('click', openOrCloseMenu);
+promoButton.addEventListener('click', openModal);
+modalButton.addEventListener('click', closeModal);
+modalWrapper.addEventListener('click', closeModal);
 
 function openOrCloseMenu() {
-  var element = document.getElementById("main-nav");
-  var typeMenu = element.classList.contains("main-nav--closed");
+  var element = document.getElementById('main-nav');
+  var typeMenu = element.classList.contains('main-nav--closed');
 
   if (typeMenu) {
     element.classList.remove('main-nav--closed');
@@ -15,4 +26,16 @@ function openOrCloseMenu() {
     navToggler.setAttribute('src', '/img/icon-menu-open.svg');
     navToggler.setAttribute('alt', 'Открыть меню');
   }
+}
+
+function openModal() {
+  var modalWindow = document.getElementById('modal');
+
+  modalWindow.classList.remove('modal--closed');
+}
+
+function closeModal() {
+  var modalWindow = document.getElementById('modal');
+
+  modalWindow.classList.add('modal--closed');
 }
