72a0688db31f1f7d07a7caaa5d422127aa6c83ee
Sergey Zveroboev <szveroboev@gmail.com>
2017-06-16 23:18:57 +0400

    Доделал слайдер

--- js/slider.js
+++ js/slider.js
@@ -4,7 +4,7 @@ var prevSlide = document.querySelector('.reviews__pagination-prev');
 
 
 nextSlide.addEventListener('click', showNextSlide);
-//prevSlide.addEventListener('click', showPrevSlide);
+prevSlide.addEventListener('click', showPrevSlide);
 
 
 function showNextSlide() {
@@ -14,10 +14,24 @@ function showNextSlide() {
       sliderItems[i].classList.add('slider__item--hidden');
       sliderItems[0].classList.remove('slider__item--hidden');
       break;
-
     } else if (!sliderItems[i].classList.contains('slider__item--hidden')) {
       sliderItems[i].classList.add('slider__item--hidden');
       sliderItems[i + 1].classList.remove('slider__item--hidden');
+      break;
+    }
+  }
+}
+
+function showPrevSlide() {
+  for (var i = sliderItems.length - 1; i >= 0; i--) {
+
+    if (i === 0) {
+      sliderItems[0].classList.add('slider__item--hidden');
+      sliderItems[sliderItems.length - 1].classList.remove('slider__item--hidden');
+      break;
+    } else if (!sliderItems[i].classList.contains('slider__item--hidden')) {
+      sliderItems[i].classList.add('slider__item--hidden');
+      sliderItems[i - 1].classList.remove('slider__item--hidden');
       break;
     }
   }
