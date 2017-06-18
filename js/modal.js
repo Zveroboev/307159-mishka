a54ec51be6258361ba63ae8b23a004abd35ec599
Sergey Zveroboev <szveroboev@gmail.com>
2017-06-18 16:52:42 +0300

    Добавил обработчик имени страницы для открытия модального окна

--- js/modal.js
+++ js/modal.js
@@ -1,16 +1,21 @@
-var openModalButton = document.getElementById('openModal');
 var closeModalButton = document.querySelector('.modal__form-send');
 var modalWindow = document.getElementById('modal');
 
-//Добавить в функцию при загрузке страницы
-openModalButton.setAttribute('href', '#');
-openModalButton.removeAttribute('target');
-//
-
-openModalButton.addEventListener('click', openModal);
 closeModalButton.addEventListener('click', closeModal);
 modalWindow.addEventListener('click', closeModal);
 
+
+if (location.pathname === '/catalog.html'){
+
+
+} else if (location.pathname === '/index.html') {
+  var openModalButton = document.getElementById('openModal');
+
+  openModalButton.setAttribute('href', '#');
+  openModalButton.removeAttribute('target');
+
+  openModalButton.addEventListener('click', openModal);
+}
 
 function openModal() {
   var modalWindow = document.getElementById('modal');
