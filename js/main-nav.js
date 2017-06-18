06eccd339f242bc66b40751108007ed6f4969e5e
Sergey Zveroboev <szveroboev@gmail.com>
2017-06-18 21:32:03 +0400

    Добавил закрытие мобильного меню через JS при загрузке страницы

--- js/main-nav.js
+++ js/main-nav.js
@@ -1,11 +1,15 @@
 var navToggler = document.querySelector('.header-top__toggler-img');
+var element = document.getElementById('main-nav');
+var typeMenu = element.classList.contains('main-nav--closed');
+
+element.classList.add('main-nav--closed');
+navToggler.setAttribute('src', '/img/icon-menu-open.svg');
+navToggler.setAttribute('alt', 'Открыть меню');
 
 navToggler.addEventListener('click', openOrCloseMenu);
 
-
 function openOrCloseMenu() {
-  var element = document.getElementById('main-nav');
-  var typeMenu = element.classList.contains('main-nav--closed');
+  typeMenu = element.classList.contains('main-nav--closed');
 
   if (typeMenu) {
     element.classList.remove('main-nav--closed');
