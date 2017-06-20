95a476d84f7c4ba8c2326feed9df9c64aaa60180
Sergey Zveroboev <szveroboev@gmail.com>
2017-06-20 15:54:59 +0300

    Добавил минификатор css, изображений, svg. Собрал и вставил спрайты.

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,31 @@
 "use strict";
 
-var gulp = require("gulp");
-var sass = require("gulp-sass");
-var plumber = require("gulp-plumber");
-var postcss = require("gulp-postcss");
-var autoprefixer = require("autoprefixer");
-var server = require("browser-sync").create();
+var gulp         = require("gulp"),
+    sass         = require("gulp-sass"),
+    plumber      = require("gulp-plumber"),
+    postcss      = require("gulp-postcss"),
+    autoprefixer = require("autoprefixer"),
+    server       = require("browser-sync").create(),
+    mqpacker     = require("css-mqpacker"),
+    minify       = require("gulp-csso"),
+    rename       = require("gulp-rename"),
+    imagemin     = require("gulp-imagemin"),
+    svgmin       = require("gulp-svgmin"),
+    svgstore     = require("gulp-svgstore");
 
 gulp.task("style", function() {
   gulp.src("sass/style.scss")
     .pipe(plumber())
     .pipe(sass())
     .pipe(postcss([
-      autoprefixer({browsers: [
+      autoprefixer({
+        browsers: [
         "last 2 versions"
       ]})
     ]))
+    .pipe(gulp.dest("css"))
+    .pipe(minify())
+    .pipe(rename("style.min.css"))
     .pipe(gulp.dest("css"))
     .pipe(server.stream());
 });
@@ -31,4 +41,34 @@ gulp.task("serve", ["style"], function() {
 
   gulp.watch("sass/**/*.{scss,sass}", ["style"]);
   gulp.watch("*.html").on("change", server.reload);
+});
+
+gulp.task("images", function() {
+  return gulp.src("img/**/*.{png,jpg,gif}")
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.jpegtran({progressive: true})
+    ]))
+    .pipe(gulp.dest("img"));
+});
+
+gulp.task("sprite", function() {
+  var cart = gulp.src("img/icon-cart.svg")
+    .pipe(svgmin())
+    .pipe(svgstore({
+      inlineSvg: true
+    }))
+    .pipe(rename("sprite-cart.svg"))
+    .pipe(gulp.dest("img"));
+
+  var social = gulp.src([
+    "img/icon-fb.svg",
+    "img/icon-twitter.svg",
+    "img/icon-insta.svg",
+    ])
+    .pipe(svgstore({
+      inlineSvg: true
+    }))
+    .pipe(rename("sprite-social.svg"))
+    .pipe(gulp.dest("img"));
 });
