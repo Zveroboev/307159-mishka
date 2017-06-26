f3ba5527627cf20c804339e031935a0c718b1eaf
Sergey Zveroboev <szveroboev@gmail.com>
2017-06-26 17:51:44 +0300

    Поправил путь до бургера в JS файле по замечанию проверяющего

--- js/main-nav.js
+++ js/main-nav.js
@@ -3,7 +3,7 @@ var element = document.getElementById('main-nav');
 var typeMenu = element.classList.contains('main-nav--closed');
 
 element.classList.add('main-nav--closed');
-navToggler.setAttribute('src', '/img/icon-menu-open.svg');
+navToggler.setAttribute('src', 'img/icon-menu-open.svg');
 navToggler.setAttribute('alt', 'Открыть меню');
 
 navToggler.addEventListener('click', openOrCloseMenu);
@@ -13,11 +13,11 @@ function openOrCloseMenu() {
 
   if (typeMenu) {
     element.classList.remove('main-nav--closed');
-    navToggler.setAttribute('src', '/img/icon-menu-close.svg');
+    navToggler.setAttribute('src', 'img/icon-menu-close.svg');
     navToggler.setAttribute('alt', 'Закрыть меню');
   } else {
     element.classList.add('main-nav--closed');
-    navToggler.setAttribute('src', '/img/icon-menu-open.svg');
+    navToggler.setAttribute('src', 'img/icon-menu-open.svg');
     navToggler.setAttribute('alt', 'Открыть меню');
   }
 }
